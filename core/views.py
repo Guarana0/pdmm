ec250be5f1c23f23e9bc1274dafd7390257469d6
Lucas <lucasgalaranidc@gmail.com>
2025-05-15 23:44:19 -0300

    adicionando o design base e tambem as views do projeto

--- core/views.py
+++ core/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+def autores(request):
+    return render(request, 'core/autores.html')
+
+def noticias(request):
+    return render(request, 'core/noticias.html')
+
+def revistas(request):
+    return render(request, 'core/revistas.html')
+
+def galeria(request):
+    return render(request, 'core/galeria.html')
+
+def livros(request):
+    return render(request, 'core/livros.html')
