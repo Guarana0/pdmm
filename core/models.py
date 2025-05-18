099fd9ca24e3a4460587fb8dd3d7fb08450fbe1d
Lucas <lucasgalaranidc@gmail.com>
2025-05-18 15:58:05 -0300

    criação do model de Autores

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Autores(models.Model):
+    id_autor = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    sobrenome = models.CharField(max_length=100)
+    data_nascimento = models.DateField(null=True, blank=True)
+    data_falecimento = models.DateField(null=True, blank=True)
+    local_nascimento = models.CharField(max_length=100, null=True, blank=True)
+    biografia = models.TextField(null=True, blank=True)
+    foto_url = models.URLField(max_length=255, null=True, blank=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nome} {self.sobrenome}"
