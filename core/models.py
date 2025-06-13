65faab14e80bfe8a4565758965b0adad2c8a8a11
Lucas Galarani <Lucasgalaranidc@gmail.com>
2025-06-13 13:13:09 -0300

    criação da pagina de admin, login, registro e configs admin

--- core/models.py
+++ core/models.py
@@ -15,3 +15,53 @@ class Autores(models.Model):
 
     def __str__(self):
         return f"{self.nome} {self.sobrenome}"
+    
+    
+
+class Livros(models.Model):
+    id_livro = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey(Autores, on_delete=models.CASCADE, related_name='livros')
+    genero = models.CharField(max_length=100)
+    ano_publicacao = models.IntegerField()
+    sinopse = models.TextField(null=True, blank=True)
+    capa_url = models.URLField(max_length=255, null=True, blank=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+    
+class Editoras(models.Model):
+    id_editora = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    localizacao = models.CharField(max_length=255, null=True, blank=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+    
+class Revistas(models.Model):
+    id_revista = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=200)
+    editora = models.ForeignKey(Editoras, on_delete=models.CASCADE, related_name='revistas')
+    edicao = models.CharField(max_length=100)
+    ano_publicacao = models.IntegerField()
+    capa_url = models.URLField(max_length=255, null=True, blank=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+    
+class Fotografos(models.Model):
+    id_fotografo = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+    
+    
+class Fotos(models.Model):
+    id_foto = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=500)
+    fotografo = models.ForeignKey(Fotografos, on_delete=models.CASCADE, related_name='fotografos')
+    local = models.CharField(max_length=200);
+    
+    data_cadastro = models.DateTimeField(auto_now_add=True)
--- pdmm/urls.py
+++ pdmm/urls.py
@@ -1,29 +1,19 @@
-"""
-URL configuration for pdmm project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
+from core import views # ou a importação que você usou
 
+# SOBRESCREVER A VIEW DE LOGIN DO ADMIN
+admin.site.login = views.MyAdminLoginView.as_view()
 
 urlpatterns = [
+    path('admin/', admin.site.urls), # Esta linha agora usará sua view de login
+    path('admin/registro', admin.site.urls),
+    
     path('', views.home, name='home'),
     path('autores/', views.autores, name='autores'),
     path('noticias/', views.noticias, name='noticias'),
     path('revistas/', views.revistas, name='revistas'),
     path('galeria/', views.galeria, name='galeria'),
     path('livros/', views.livros, name='livros'),
+    # ... outras URLs ...
 ]
