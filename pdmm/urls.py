a337bb501f69aface7775cd826c492be4ebc9f8f
Lucas Galarani <Lucasgalaranidc@gmail.com>
2025-06-28 23:15:05 -0300

    resolvendo conflito no urls.py

--- pdmm/urls.py
+++ pdmm/urls.py
@@ -2,30 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from core import views # ou a importação que você usou
 
-# SOBRESCREVER A VIEW DE LOGIN DO ADMIN
 admin.site.login = views.MyAdminLoginView.as_view()
 
 urlpatterns = [
-    # Acessa dos Admins
-    path('admin/', admin.site.urls), # Esta linha agora usará sua view de login
-<<<<<<< HEAD
-    path("accounts/", include("django.contrib.auth.urls")),
-    
-    # Acesso geral da pagina
-    path('', views.home, name='home'),
-    
-     path('autores/', views.autores, name='autores'),
-     path('autores/<slug:slug>/', views.autor_detail, name='autor_detail'),
-    
-=======
-    path('registro/', views.registro),
-    path('login/', views.login_view),
-     path('', include('core.urls')),
-    path('autores/', views.autores, name='autores'),
->>>>>>> origin/main
-    path('noticias/', views.noticias, name='noticias'),
-    path('revistas/', views.revistas, name='revistas'),
-    path('galeria/', views.galeria, name='galeria'),
-    path('livros/', views.livros, name='livros'),
-    # ... outras URLs ...
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
