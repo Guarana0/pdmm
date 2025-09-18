4aa3c9c7b9a1b04b1f148dd3c1669aeed808417a
Lucas Galarani <Lucasgalaranidc@gmail.com>
2025-09-18 13:34:57 -0300

    ajustes na galeria e começo da criação e desenvolvimento da página de admin

--- pdmm/urls.py
+++ pdmm/urls.py
@@ -10,3 +10,6 @@ urlpatterns = [
     path('admin/login/', views.MyAdminLoginView.as_view(), name='admin_login'),
     path('admin/password_change/', views.MyAdminPasswordChangeView.as_view(), name='admin_password_change'),
 ]
+
+# Custom 404 handler (uses templates/404.html). Only active when DEBUG = False.
+handler404 = 'django.views.defaults.page_not_found'
